0380f35a78e1f473f1de15e49c514cfb28b853e4
dongjk <hikihomori@gmail.com>
2018-05-26 20:43:43 +0800

    initial

---
+++ RPN.py
@@ -0,0 +1,181 @@
+import numpy as np
+import numpy.random as npr
+from keras.layers import Conv2D
+from keras.models import Input, Model
+from keras.applications import InceptionResNetV2
+from keras.preprocessing.image import load_img, img_to_array
+from utils import generate_anchors, draw_anchors, bbox_overlaps, bbox_transform,\
+                    loss_cls, smoothL1, parse_label, unmap
+
+k=9 #anchor number for each point
+##################  RPN Model  #######################
+feature_map_tile = Input(shape=(None,None,1536))
+convolution_3x3 = Conv2D(
+    filters=512,
+    kernel_size=(3, 3),
+    padding='same',
+    name="3x3"
+)(feature_map_tile)
+
+output_deltas = Conv2D(
+    filters= 4 * k,
+    kernel_size=(1, 1),
+    activation="linear",
+    kernel_initializer="uniform",
+    name="deltas1"
+)(convolution_3x3)
+
+output_scores = Conv2D(
+    filters=1 * k,
+    kernel_size=(1, 1),
+    activation="sigmoid",
+    kernel_initializer="uniform",
+    name="scores1"
+)(convolution_3x3)
+
+model = Model(inputs=[feature_map_tile], outputs=[output_scores, output_deltas])
+model.compile(optimizer='adam', loss={'scores1':loss_cls, 'deltas1':smoothL1})
+
+##################  prepare batch  #######################
+BG_FG_FRAC=2
+
+#load an example to void graph problem
+#TODO fix this.
+pretrained_model = InceptionResNetV2(include_top=False)
+img=load_img("./ILSVRC2014_train_00010391.JPEG")
+x = img_to_array(img)
+x = np.expand_dims(x, axis=0)
+not_used=pretrained_model.predict(x)
+
+def produce_batch(filepath, gt_boxes):
+    img=load_img(filepath)
+    img_width=np.shape(img)[1]
+    img_height=np.shape(img)[0]
+    #feed image to pretrained model and get feature map
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    feature_map=pretrained_model.predict(img)
+    height = np.shape(feature_map)[1]
+    width = np.shape(feature_map)[2]
+    num_feature_map=width*height
+    #calculate output w, h stride
+    w_stride = img_width / width
+    h_stride = img_height / height
+    #generate base anchors according output stride.
+    #base anchors are 9 anchors wrt a tile (0,0,w_stride-1,h_stride-1)
+    base_anchors=generate_anchors(w_stride,h_stride)
+    #slice tiles according to image size and stride.
+    #each 1x1x1532 feature map is mapping to a tile.
+    shift_x = np.arange(0, width) * w_stride
+    shift_y = np.arange(0, height) * h_stride
+    shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+    shifts = np.vstack((shift_x.ravel(), shift_y.ravel(), shift_x.ravel(), 
+                            shift_y.ravel())).transpose()
+    #apply base anchors to all tiles, to have a num_feature_map*9 anchors.
+    all_anchors = (base_anchors.reshape((1, 9, 4)) +
+                    shifts.reshape((1, num_feature_map, 4)).transpose((1, 0, 2)))
+    total_anchors = num_feature_map*9
+    all_anchors = all_anchors.reshape((total_anchors, 4))
+    #only keep anchors inside image+borader.
+    border=0
+    inds_inside = np.where(
+            (all_anchors[:, 0] >= -border) &
+            (all_anchors[:, 1] >= -border) &
+            (all_anchors[:, 2] < img_width+border ) &  # width
+            (all_anchors[:, 3] < img_height+border)    # height
+    )[0]
+    anchors=all_anchors[inds_inside]
+    # calculate overlaps each anchors to each gt boxes, 
+    # a matrix with shape [len(anchors) x len(gt_boxes)]
+    overlaps = bbox_overlaps(anchors, gt_boxes)
+    # find the gt box with biggest overlap to each anchors, 
+    # and the overlap ratio. result (len(anchors),)
+    argmax_overlaps = overlaps.argmax(axis=1)
+    max_overlaps = overlaps[np.arange(len(inds_inside)), argmax_overlaps]
+    # find the anchor with biggest overlap to each gt boxes, 
+    # and the overlap ratio. result (len(gt_boxes),)
+    gt_argmax_overlaps = overlaps.argmax(axis=0)
+    gt_max_overlaps = overlaps[gt_argmax_overlaps,
+                                np.arange(overlaps.shape[1])]
+    gt_argmax_overlaps = np.where(overlaps == gt_max_overlaps)[0]
+    #labels, 1=fg/0=bg/-1=ignore
+    labels = np.empty((len(inds_inside), ), dtype=np.float32)
+    labels.fill(-1)
+    # set positive label, define in Paper3.1.2: 
+    # We assign a positive label to two kinds of anchors: (i) the
+    # anchor/anchors with the highest Intersection-overUnion
+    # (IoU) overlap with a ground-truth box, or (ii) an
+    # anchor that has an IoU overlap higher than 0.7 with any gt boxes
+    labels[gt_argmax_overlaps] = 1
+    labels[max_overlaps >= .7] = 1
+    # set negative labels
+    labels[max_overlaps <= .3] = 0
+    # subsample positive labels if we have too many
+#     num_fg = int(RPN_FG_FRACTION * RPN_BATCHSIZE)
+    fg_inds = np.where(labels == 1)[0]
+#     if len(fg_inds) > num_fg:
+#         disable_inds = npr.choice(
+#             fg_inds, size=(len(fg_inds) - num_fg), replace=False)
+#         labels[disable_inds] = -1
+    # subsample negative labels if we have too many
+    num_bg = int(len(fg_inds) * BG_FG_FRAC)
+    bg_inds = np.where(labels == 0)[0]
+    if len(bg_inds) > num_bg:
+        disable_inds = npr.choice(
+            bg_inds, size=(len(bg_inds) - num_bg), replace=False)
+        labels[disable_inds] = -1
+    #
+    batch_inds=inds_inside[labels!=-1]
+    batch_inds=(batch_inds / k).astype(np.int)
+    full_labels = unmap(labels, total_anchors, inds_inside, fill=-1)
+    batch_label_targets=full_labels.reshape(-1,1,1,1*k)[batch_inds]
+    bbox_targets = np.zeros((len(inds_inside), 4), dtype=np.float32)
+    # bbox_targets = bbox_transform(anchors, gt_boxes[argmax_overlaps, :]
+    pos_anchors=all_anchors[inds_inside[labels==1]]
+    bbox_targets = bbox_transform(pos_anchors, gt_boxes[argmax_overlaps, :][labels==1])
+    bbox_targets = unmap(bbox_targets, total_anchors, inds_inside[labels==1], fill=0)
+    batch_bbox_targets = bbox_targets.reshape(-1,1,1,4*k)[batch_inds]
+    padded_fcmap=np.pad(feature_map,((0,0),(1,1),(1,1),(0,0)),mode='constant')
+    padded_fcmap=np.squeeze(padded_fcmap)
+    batch_tiles=[]
+    for ind in batch_inds:
+        x = ind % width
+        y = int(ind/width)
+        fc_3x3=padded_fcmap[y:y+3,x:x+3,:]
+        batch_tiles.append(fc_3x3)
+    return np.asarray(batch_tiles), batch_label_targets.tolist(), batch_bbox_targets.tolist()
+
+ILSVRC_dataset_path='/home/jk/wi/ILSVRC/'
+img_path=ILSVRC_dataset_path+'Data/DET/train/'
+anno_path=ILSVRC_dataset_path+'/Annotations/DET/train/'
+import glob
+
+BATCH_SIZE=256
+def input_generator():
+    batch_tiles=[]
+    batch_labels=[]
+    batch_bboxes=[]
+    while 1:
+        for fname in glob.glob(ILSVRC_dataset_path+'/ImageSets/DET/train_*'):
+            with open(fname,'r') as f:
+                for line in f:
+                    if 'extra' not in line:
+                        try:
+                            category, gt_boxes = parse_label(anno_path+line.split()[0]+'.xml')
+                            tiles, labels, bboxes = produce_batch(img_path+line.split()[0]+'.JPEG', gt_boxes)
+                        except Exception:
+                            print('parse label or produce batch failed: for: '+line.split()[0])
+                            continue
+                        for i in range(len(tiles)):
+                            batch_tiles.append(tiles[i])
+                            batch_labels.append(labels[i])
+                            batch_bboxes.append(bboxes[i])
+                            if(len(batch_tiles)==BATCH_SIZE):
+                                yield np.asarray(batch_tiles), [np.asarray(batch_labels), np.asarray(batch_bboxes)]
+                                batch_tiles=[]
+                                batch_labels=[]
+                                batch_bboxes=[]
+
+
+
+model.fit_generator(input_generator(), steps_per_epoch=388448, epochs=1)
---
+++ utils.py
@@ -0,0 +1,202 @@
+import sys
+import numpy as np
+import keras.backend as K
+import xml.etree.ElementTree as ET
+from PIL import Image, ImageDraw
+
+def parse_label(xml_file):
+    try:
+        tree = ET.parse(xml_file)
+    except Exception:
+        print('Failed to parse: ' + xml_file, file=sys.stderr)
+        return None
+    root = tree.getroot()
+    category=[]
+    xmin=[]
+    ymin=[]
+    xmax=[]
+    ymax=[]
+    for x in root.iter('name'):
+        category.append(x.text)
+    for x in root.iter('xmin'):
+        xmin.append(x.text)
+    for x in root.iter('ymin'):
+        ymin.append(x.text)
+    for x in root.iter('xmax'):
+        xmax.append(x.text)
+    for x in root.iter('ymax'):
+        ymax.append(x.text)
+    gt_boxes=[list(box) for box in zip(xmin,ymin,xmax,ymax)]
+    return category, np.asarray(gt_boxes, np.float)
+
+
+def loss_cls(y_true, y_pred):
+    print(y_true,y_pred)
+    output_scores = K.reshape(y_pred, (1, -1))
+    y_true = K.reshape(y_true, (1, -1))
+
+    condition = K.not_equal(y_true, -1)
+    indices = K.tf.where(condition)
+    indices = K.expand_dims(indices, 0)
+
+    target = K.tf.gather_nd(y_true, indices)
+    output = K.tf.gather_nd(output_scores, indices)
+    loss = K.binary_crossentropy(target, output)
+    return loss
+
+
+def smoothL1(y_true, y_pred):
+    nd=K.tf.where(K.tf.not_equal(y_true,0))
+    y_true=K.tf.gather_nd(y_true,nd)
+    y_pred=K.tf.gather_nd(y_pred,nd)
+    x   = K.tf.losses.huber_loss(y_true,y_pred)
+#     x   = K.switch(x < HUBER_DELTA, 0.5 * x ** 2, HUBER_DELTA * (x - 0.5 * HUBER_DELTA))
+    return  K.sum(x)
+
+
+def draw_anchors(img_path, anchors, pad_size=50):
+    im = Image.open(img_path)
+    w,h=im.size
+    a4im = Image.new('RGB',
+                    (w+2*pad_size, h+2*pad_size),   # A4 at 72dpi
+                    (255, 255, 255))  # White
+    a4im.paste(im, (pad_size,pad_size))  # Not centered, top-left corner
+    for a in anchors:
+        a=(a+pad_size).astype(int).tolist()
+        draw = ImageDraw.Draw(a4im)
+        draw.rectangle(a,outline=(255,0,0), fill=None)
+    return a4im
+
+def generate_anchors(base_width=16, base_height=16, ratios=[0.5, 1, 2],
+                     scales=np.asarray([3,6,12])):
+    """
+    Generate anchor (reference) windows by enumerating aspect ratios X
+    scales wrt a reference (0, 0, w_stride-1, h_stride-1) window.
+    """
+
+    base_anchor = np.array([1, 1, base_width, base_height]) - 1
+    ratio_anchors = _ratio_enum(base_anchor, ratios)
+    anchors = np.vstack([_scale_enum(ratio_anchors[i, :], scales)
+                         for i in range(ratio_anchors.shape[0])])
+    return anchors
+
+def _whctrs(anchor):
+    """
+    Return width, height, x center, and y center for an anchor (window).
+    """
+
+    w = anchor[2] - anchor[0] + 1
+    h = anchor[3] - anchor[1] + 1
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    y_ctr = anchor[1] + 0.5 * (h - 1)
+    return w, h, x_ctr, y_ctr
+
+def _mkanchors(ws, hs, x_ctr, y_ctr):
+    """
+    Given a vector of widths (ws) and heights (hs) around a center
+    (x_ctr, y_ctr), output a set of anchors (windows).
+    """
+
+    ws = ws[:, np.newaxis]
+    hs = hs[:, np.newaxis]
+    anchors = np.hstack((x_ctr - 0.5 * (ws - 1),
+                         y_ctr - 0.5 * (hs - 1),
+                         x_ctr + 0.5 * (ws - 1),
+                         y_ctr + 0.5 * (hs - 1)))
+    return anchors
+
+def _ratio_enum(anchor, ratios):
+    """
+    Enumerate a set of anchors for each aspect ratio wrt an anchor.
+    """
+
+    w, h, x_ctr, y_ctr = _whctrs(anchor)
+    size = w * h
+    size_ratios = size / ratios
+    ws = np.round(np.sqrt(size_ratios))
+    hs = np.round(ws * ratios)
+    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+    return anchors
+
+def _scale_enum(anchor, scales):
+    """
+    Enumerate a set of anchors for each scale wrt an anchor.
+    """
+
+    w, h, x_ctr, y_ctr = _whctrs(anchor)
+    ws = w * scales
+    hs = h * scales
+    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+    return anchors
+
+if __name__ == '__main__':
+    import time
+    t = time.time()
+    a = generate_anchors(37,37)
+    print(a)
+
+
+
+def bbox_transform(ex_rois, gt_rois):
+    ex_widths = ex_rois[:, 2] - ex_rois[:, 0] + 1.0
+    ex_heights = ex_rois[:, 3] - ex_rois[:, 1] + 1.0
+    ex_ctr_x = ex_rois[:, 0] + 0.5 * ex_widths
+    ex_ctr_y = ex_rois[:, 1] + 0.5 * ex_heights
+
+    gt_widths = gt_rois[:, 2] - gt_rois[:, 0] + 1.0
+    gt_heights = gt_rois[:, 3] - gt_rois[:, 1] + 1.0
+    gt_ctr_x = gt_rois[:, 0] + 0.5 * gt_widths
+    gt_ctr_y = gt_rois[:, 1] + 0.5 * gt_heights
+
+    targets_dx = (gt_ctr_x - ex_ctr_x) / ex_widths
+    targets_dy = (gt_ctr_y - ex_ctr_y) / ex_heights
+    targets_dw = np.log(gt_widths / ex_widths)
+    targets_dh = np.log(gt_heights / ex_heights)
+
+    targets = np.stack((targets_dx, targets_dy, targets_dw, targets_dh))
+
+    targets = np.transpose(targets)
+
+    return targets
+
+def bbox_overlaps(boxes, query_boxes):
+    """
+    Parameters
+    ----------
+    boxes: (N, 4) ndarray of float
+    query_boxes: (K, 4) ndarray of float
+    Returns
+    -------
+    overlaps: (N, K) ndarray of overlap between boxes and query_boxes
+    """
+    boxes=boxes.astype(int)
+    N = boxes.shape[0]
+    K = query_boxes.shape[0]
+
+    overlaps = np.zeros((N, K), dtype=np.float)
+
+    for k in range(K):
+        box_area = ((query_boxes[k, 2] - query_boxes[k, 0] + 1) * (query_boxes[k, 3] - query_boxes[k, 1] + 1))
+        for n in range(N):
+            iw = (min(boxes[n, 2], query_boxes[k, 2]) - max(boxes[n, 0], query_boxes[k, 0]) + 1)
+            if iw > 0:
+                ih = (min(boxes[n, 3], query_boxes[k, 3]) - max(boxes[n, 1], query_boxes[k, 1]) + 1)
+
+                if ih > 0:
+                    ua = float((boxes[n, 2] - boxes[n, 0] + 1) * (boxes[n, 3] - boxes[n, 1] + 1) + box_area - iw * ih)
+                    overlaps[n, k] = iw * ih / ua
+
+    return overlaps
+
+def unmap(data, count, inds, fill=0):
+    """ Unmap a subset of item (data) back to the original set of items (of
+    size count) """
+    if len(data.shape) == 1:
+        ret = np.empty((count, ), dtype=np.float32)
+        ret.fill(fill)
+        ret[inds] = data
+    else:
+        ret = np.empty((count, ) + data.shape[1:], dtype=np.float32)
+        ret.fill(fill)
+        ret[inds, :] = data
+    return ret
